fafd25fec5c9df6ccbda79e47f230fdda8a42dd8
azimi4002 <azimi4002@gmail.com>
2026-03-30 00:03:10 +1100

    change directories

--- src/test_tile.py
+++ src/test_tile.py
@@ -1,11 +1,8 @@
 import unittest
-from typing import Dict, Tuple, List
 
-from board import MAIN_POSITIONS
 from colors import Color
-from sample_tiles import MAIN_TILES, SINGLE_COLOR_TILES, TWO_TILES
-from solver import solve, is_compatible
 from tile import Tile
+
 
 class TestTile(unittest.TestCase):
     def test_two_color_cross_rotations(self):
